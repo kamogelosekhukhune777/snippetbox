a0dc839232146881a80a37c9788c283a9b9be4a5
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2024-06-16 22:40:36 +0200

    executing SQL statements

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -38,7 +38,19 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a new snippet..."))
+	// Create some variables holding dummy data. We'll remove these later on
+	// during the build.
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	// Redirect the user to the relevant page for the snippet.
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
