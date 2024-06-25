9bac0c374d2817217606f82e7d4780872d52196e
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2024-06-25 16:51:32 +0200

    setting yp the session manager

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 // The routes() method returns a servemux containing our application routes.
 func (app *application) routes() http.Handler {
@@ -9,10 +13,21 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
+	// Create a new middleware chain containing the middleware specific to our
+	// dynamic application routes. For now, this chain will only contain the
+	// LoadAndSave session middleware but we'll add more to it later.
+	dynamic := alice.New(app.sessionManager.LoadAndSave)
+	// Update these routes to use the new dynamic middleware chain followed by
+	// the appropriate handler function. Note that because the alice ThenFunc()
+	// method returns a http.Handler (rather than a http.HandlerFunc) we also
+	// need to switch to registering the route using the mux.Handle() method.
+	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
+
 	mux.HandleFunc("GET /{$}", app.home)
 	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+	return standard.Then(mux)
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/kamogelosekhukhune777/snippetbox
 go 1.22.1
 
 require (
+	github.com/alexedwards/scs/mysqlstore v0.0.0-20240316134038-7e11d57e8885
+	github.com/alexedwards/scs/v2 v2.8.0
 	github.com/go-playground/form/v4 v4.2.1
 	github.com/go-sql-driver/mysql v1.8.1
+	github.com/justinas/alice v1.2.0
 )
 
 require filippo.io/edwards25519 v1.1.0 // indirect
